9095880ad6a56bd49bea42c66ba8b0a8f89e5192
ajor-saha <sokarama79@gmail.com>
2024-02-23 14:39:54 +0600

    update

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello, Vercel!");
 });
 
