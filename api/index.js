9dae584c34980adf84f10510aacdfe9b988fef8f
ajor-saha <sokarama79@gmail.com>
2024-03-21 18:36:51 +0600

    update

--- api/index.js
+++ api/index.js
@@ -1,35 +1,20 @@
-import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import connectDB from "./db/index.js";
-import userRouter from "./routes/user.route.js";
+import dotenv from "dotenv"
+import { app } from "./src/app.js";
+import connectDB from "./src/db/index.js";
+
 dotenv.config({
-  path: "./.env",
+    path: './.env'
+})
+
+
+
+connectDB();
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
 });
 
-const app = express();
-
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
-
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello, Vercel!");
 });
-
-connectDB()
-
-
-app.listen(8000, () => {
-    console.log('Server is running on port 8000!');
-  });
-  
---
+++ client/src/pages/CheckoutSuccess.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CheckoutSuccess = () => {
+  return (
+    <div className='py-20'>
+      <h2>Checkout Successfull</h2>
+    </div>
+  )
+}
+
+export default CheckoutSuccess
